a4f15bfc6ea6ab6b5a3af2281e8f76ccd9807532
LUXOdi7 <pajaron202018@gmail.com>
2025-04-14 13:09:06 -0500

    bd_postgres corregido

--- db_setup/create-postgres.sql
+++ db_setup/create-postgres.sql
@@ -1,122 +1,122 @@
 create table contrato (
-    id varchar,
+    contrato_id serial,
     servicio_id int,
     cliente_id int,
     prestador_id int,
-    estado varchar(20),
-    fecha_inicio timestamp,
-    fecha_finalizacion timestamp,
-    primary key (id)
+    estado varchar(20) CHECK (estado IN ('pendiente', 'en progreso', 'completado', 'cancelado')) DEFAULT 'pendiente',
+    fecha_inicio date,
+    fecha_finalizacion date,
+    primary key (contrato_id)
 );
 
 create table usuario (
-    id varchar,
+    usuario_id serial,
     email varchar(100) not null unique,
-    password_hash text not null,
-    fecha_creacion timestamp,
+    password_hash varchar(64) not null,
+    fecha_creacion date,
     admin boolean not null,
-    url_picture varchar,
-    primary key (id)
+    url_picture varchar(40),
+    primary key (usuario_id)
 );
 
 create table comprobante_contrato (
-    id varchar,
+    comprobante_contrato_id serial,
     contrato_id int,
-    monto decimal not null,
+    monto numeric(7,2) not null,
     metodo_pago_id int,
-    fecha_pago timestamp,
-    primary key (id)
+    fecha_pago date,
+    primary key (comprobante_contrato_id)
 );
 
 create table comentario (
-    id varchar,
+    comentario_id serial,
     contrato_id int,
     calificacion int,
-    comentario text,
-    fecha_creacion timestamp,
-    primary key (id)
+    comentario varchar(255),
+    fecha_creacion date,
+    primary key (comentario_id)
 );
 
 create table persona (
-    id varchar,
+    persona_id serial,
     usuario_id int unique,
     nombre varchar(100) not null,
     apellido varchar(100) not null,
     fecha_nacimiento date,
-    primary key (id)
+    primary key (persona_id)
 );
 
 
 
 create table metodo_pago (
-    id varchar,
+    metodo_pago_id serial,
     nombre varchar(50) not null unique,
-    primary key (id)
+    primary key (metodo_pago_id)
 );
 
 create table publicacion_medio (
-    id integer,
+    publicacion_medio_id serial,
     pulicacion_id integer not null,
     tamanio integer,
-    time timestamp with time zone,
-    primary key (id)
+    fecha_hora timestamp,
+    primary key (publicacion_medio_id)
 );
 
 create table empresa (
-    id varchar,
+    empresa_id serial,
     usuario_id int unique,
     nombre varchar(255) not null unique,
-    descripcion text,
-    fecha_creacion timestamp,
-    primary key (id)
+    descripcion varchar(255),
+    fecha_creacion date,
+    primary key (empresa_id)
 );
 
 create table categoria (
-    id varchar,
+    categoria_id serial,
     nombre varchar(100) not null unique,
-    primary key (id)
+    primary key (categoria_id)
 );
 
 create table publicacion (
-    id integer,
+    publicacion_id serial,
     categoria_id integer,
     usuario_id integer,
-    titulo varchar(255) not null,
-    descripcion text not null,
+    titulo varchar(80) not null,
+    descripcion varchar(255) not null,
     precio decimal not null,
-    fecha_creacion timestamp,
-    primary key (id)
+    fecha_creacion date,
+    primary key (publicacion_id)
 );
 
 alter table comentario
-add constraint fk_comentario_contrato_id_contrato_id foreign key(contrato_id) references contrato(id);
+add constraint fk_comentario_contrato_id_contrato_id foreign key(contrato_id) references contrato(contrato_id);
 
 alter table comprobante_contrato
-add constraint fk_comprobante_contrato_contrato_id_contrato_id foreign key(contrato_id) references contrato(id);
+add constraint fk_comprobante_contrato_contrato_id_contrato_id foreign key(contrato_id) references contrato(contrato_id);
 
 alter table comprobante_contrato
-add constraint fk_comprobante_contrato_metodo_pago_id_metodo_pago_id foreign key(metodo_pago_id) references metodo_pago(id);
+add constraint fk_comprobante_contrato_metodo_pago_id_metodo_pago_id foreign key(metodo_pago_id) references metodo_pago(metodo_pago_id);
 
 alter table contrato
-add constraint fk_contrato_cliente_id_usuario_id foreign key(cliente_id) references usuario(id);
+add constraint fk_contrato_cliente_id_usuario_id foreign key(cliente_id) references usuario(usuario_id);
 
 alter table contrato
-add constraint fk_contrato_prestador_id_usuario_id foreign key(prestador_id) references usuario(id);
+add constraint fk_contrato_prestador_id_usuario_id foreign key(prestador_id) references usuario(usuario_id);
 
 alter table contrato
-add constraint fk_contrato_servicio_id_publicacion_id foreign key(servicio_id) references publicacion(id);
+add constraint fk_contrato_servicio_id_publicacion_id foreign key(servicio_id) references publicacion(publicacion_id);
 
 alter table empresa
-add constraint fk_empresa_usuario_id_usuario_id foreign key(usuario_id) references usuario(id);
+add constraint fk_empresa_usuario_id_usuario_id foreign key(usuario_id) references usuario(usuario_id);
 
 alter table persona
-add constraint fk_persona_usuario_id_usuario_id foreign key(usuario_id) references usuario(id);
+add constraint fk_persona_usuario_id_usuario_id foreign key(usuario_id) references usuario(usuario_id);
 
 alter table publicacion
-add constraint fk_publicacion_categoria_id_categoria_id foreign key(categoria_id) references categoria(id);
+add constraint fk_publicacion_categoria_id_categoria_id foreign key(categoria_id) references categoria(categoria_id);
 
 alter table publicacion
-add constraint fk_publicacion_usuario_id_usuario_id foreign key(usuario_id) references usuario(id);
+add constraint fk_publicacion_usuario_id_usuario_id foreign key(usuario_id) references usuario(usuario_id);
 
 alter table publicacion_medio
-add constraint fk_publicacion_medio_pulicacion_id_publicacion_id foreign key(pulicacion_id) references publicacion(id);
+add constraint fk_publicacion_medio_pulicacion_id_publicacion_id foreign key(pulicacion_id) references publicacion(publicacion_id);
