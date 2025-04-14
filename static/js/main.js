7654c5359d0bfdee609a2fce4e9b33e71b74673c
juanusat <71309030@usat.pe>
2025-04-13 20:24:27 -0500

    Plantilla asíncrona

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+(function () {
+    const appElement = document.querySelector('.app');
+    let headerElement = document.createElement('div');
+    headerElement.classList.add('header');
+
+    let footerElement = document.createElement('div');
+    footerElement.classList.add('footer');
+
+    Promise.all([
+        fetch('/templates/header.html').then(res => res.text()),
+        fetch('/templates/footer.html').then(res => res.text())
+    ]).then(([headerHTML, footerHTML]) => {
+        headerElement.innerHTML = headerHTML;
+        footerElement.innerHTML = footerHTML;
+
+        appElement.insertBefore(headerElement, appElement.firstChild);
+        appElement.appendChild(footerElement);
+
+        appElement.classList.remove('loading');
+    }).catch(err => {
+        console.error('Error cargando plantillas:', err);
+    });
+})();
