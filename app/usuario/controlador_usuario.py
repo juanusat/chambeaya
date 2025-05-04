e9529c5c12e4281c58d18a11e7bdaaada2fcb7c7
Josias Sandoval <josisan3108@gmail.com>
2025-05-03 20:14:18 -0500

    RUTA-Modificar datos usaario y validar username

--- app/usuario/controlador_usuario.py
+++ app/usuario/controlador_usuario.py
@@ -49,6 +49,51 @@ def update_usuario(user_id, data):
     finally:
         conn.close()
 
+def update_usuario_persona(persona_id, data):
+    conn = get_db_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                UPDATE persona
+                SET nombre=%s,
+                    apellido=%s,
+                    telefono=%s,
+                    fecha_nacimiento=%s
+                WHERE persona_id=%s;
+                """,
+                (
+                    data['nombre'],
+                    data['apellido'],
+                    data['telefono'],
+                    data['fecha_nacimiento'],
+                    persona_id
+                )
+            )
+            conn.commit()
+    finally:
+        conn.close()
+
+def update_usuario_empresa(empresa_id, data):
+    conn = get_db_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                UPDATE empresa
+                SET descripcion=%s,
+                    fecha_creacion=%s
+                WHERE empresa_id=%s;
+                """,
+                (
+                    data['descripcion'],
+                    data['fecha_creacion'],
+                    empresa_id
+                )
+            )
+            conn.commit()
+    finally:
+        conn.close()
 
 def delete_usuario(user_id):
     conn = get_db_connection()
@@ -68,6 +113,18 @@ def get_usuario_by_username(username):
                 "usu.email, per.fecha_nacimiento, usu.url_picture FROM usuario usu "
                 "INNER JOIN persona per ON per.usuario_id = usu.usuario_id "
                 "WHERE username=%s;",
+                (username,)
+            )
+            return cursor.fetchone()
+    finally:
+        conn.close()
+
+def get_validar_username_usuario(username):
+    conn = get_db_connection()
+    try:
+        with conn.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute(
+                "SELECT EXISTS (SELECT 1 FROM usuario WHERE username = %s) AS encontrado",
                 (username,)
             )
             return cursor.fetchone()
