e602d0fe28dac84e3c80f55da28f2c3e60b50b88
juanusat <71309030@usat.pe>
2025-05-14 11:53:37 -0500

    Remover comentarios

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import os
 import threading
 import ssl
-
 from app import create_app
 
 app = create_app()
 
 def run_http():
-    # desactivamos reloader para que no intente instalar señales en un hilo
     app.run(debug=True,
             host='0.0.0.0',
             port=4250,
@@ -16,7 +14,6 @@ def run_http():
 def run_https():
     ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     ssl_ctx.load_cert_chain('cert/cert.pem', 'cert/key.pem')
-    # desactivamos reloader también aquí
     app.run(debug=True,
             host='0.0.0.0',
             port=4251,
@@ -25,10 +22,8 @@ def run_https():
 
 if __name__ == '__main__':
     if os.getenv('PYTHONANYWHERE_DOMAIN'):
-        # en PA solo HTTP, y PA ya gestiona su propio WSGI/reloader
         run_http()
     else:
-        # en local lanzamos HTTP + HTTPS
         t1 = threading.Thread(target=run_http)
         t2 = threading.Thread(target=run_https)
         t1.start()
