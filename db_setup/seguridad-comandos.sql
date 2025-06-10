283f08978deed739d2c922410a59d71040119964
juanusat <71309030@usat.pe>
2025-06-10 15:22:22 -0500

    ddl de la nuev tabla

---
+++ db_setup/seguridad-comandos.sql
@@ -0,0 +1,9 @@
+SELECT 
+    sesiones.id AS sesion_id,
+    sesiones.usuario_id,
+    usuarios.username,
+    usuarios.email,
+    sesiones.clave_hash,
+    sesiones.creado_en AS sesion_creada_en
+FROM sesiones
+JOIN usuarios ON sesiones.usuario_id = usuarios.id;
